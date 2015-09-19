e1bec44c65d6688f94d518efeb15facc3558da44
Shahram <nikbakhtian@gmail.com>
2015-09-19 18:26:21 +0100

    Import manually the code from hello

---
+++ src/test/java/com/sandh/billsplitter/domain/BillTest.java
@@ -0,0 +1,33 @@
+package com.sandh.billsplitter.domain;
+
+/**
+ * Created by user on 19/09/15.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BillTest {
+
+    public static final String KALAMARY = "kalamary";
+    public static final int ITEMPRICE = -5;
+    public static final int NUM_ITEMS = 2;
+
+
+    @Test
+    public void testBillAddItem() {
+        Item item = new Item();
+        item.setPrice(ITEMPRICE);
+
+        Item item2 = new Item();
+        item2.setPrice(ITEMPRICE);
+
+        Bill bill = new Bill();
+
+        bill.addItem(item);
+        bill.addItem(item2);
+
+        Assert.assertTrue("Bil should have " + NUM_ITEMS, NUM_ITEMS == bill.getNumberItems());
+
+    }
+}
---
+++ src/test/java/com/sandh/billsplitter/services/BillUtilityTest.java
@@ -0,0 +1,77 @@
+package com.sandh.billsplitter.services;
+
+/**
+ * Created by user on 19/09/15.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BillUtilityTest {
+    public static final String SHOULD_PAY = " should pay ";
+    private final BillTestScenario billTestScenario = new BillTestScenario();
+    private BillUtility billUtility = new BillUtility();
+
+    @Test
+    public void calculateTotalTest() {
+        BillTestScenario.BillScenario billScenario = billTestScenario.simpleBill();
+        double total = billUtility.calculateTotal(billScenario.getBill());
+        double expectedTotal = BillTestScenario.KOOBIDEH_PRICE + BillTestScenario.JOOJEH_PRICE + BillTestScenario.GHORMEH_PRICE;
+        Assert.assertEquals("Total should be ", expectedTotal, total, 0.00001);
+    }
+
+    private void runAsserts(BillTestScenario.BillScenario billScenario)
+    {
+        Map participants = billUtility.calculateTotalPerPerson(billScenario.getBill());
+        for (Map.Entry<String, Double> assertion : billScenario.getAsserts().entrySet())
+        {
+            String name = assertion.getKey();
+            Object share = participants.get(name);
+            Assert.assertNotNull(name + " is missing",share);
+            Assert.assertTrue(name + SHOULD_PAY, (double) share == assertion.getValue());
+        }
+    }
+
+    @Test
+    public void splitBill(){
+        BillTestScenario.BillScenario billScenario = billTestScenario.simpleBill();
+        runAsserts(billScenario);
+    }
+
+    @Test
+    public void simpleBillDisclaim(){
+        BillTestScenario.BillScenario billScenario = billTestScenario.simpleBillDisclaim();
+        runAsserts(billScenario);
+    }
+
+    @Test
+    public void splitSharedBill(){
+        BillTestScenario.BillScenario billScenario = billTestScenario.simpleBillShared();
+        runAsserts(billScenario);
+    }
+
+    @Test
+    public void ownedByHamedLaterSharedByShahram(){
+        BillTestScenario.BillScenario billScenario = billTestScenario.ownedByHamedLaterSharedByShahram();
+        runAsserts(billScenario);
+    }
+
+    @Test
+    public void sameItemNameOwnedbyDifferentGroups(){
+        BillTestScenario.BillScenario billScenario = billTestScenario.sameItemnameOwnedbyDifferentGroups();
+        runAsserts(billScenario);
+    }
+
+    @Test
+    public void sharedBillDisclaim(){
+        BillTestScenario.BillScenario billScenario = billTestScenario.sharedBillDisclaim();
+        runAsserts(billScenario);
+    }
+}
+
+
