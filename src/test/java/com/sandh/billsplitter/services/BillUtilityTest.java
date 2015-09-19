252fb385e8cb9aff5d947a3731056a91fd27e4b6
Shahram <nikbakhtian@gmail.com>
2015-09-19 18:39:56 +0100

    remove the code which I dont understand :)

--- src/test/java/com/sandh/billsplitter/services/BillUtilityTest.java
+++ src/test/java/com/sandh/billsplitter/services/BillUtilityTest.java
@@ -6,12 +6,8 @@ package com.sandh.billsplitter.services;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import java.util.Map;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 public class BillUtilityTest {
     public static final String SHOULD_PAY = " should pay ";
     private final BillTestScenario billTestScenario = new BillTestScenario();
